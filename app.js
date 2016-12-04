a97c376475264031e487be34646e9b9e2e9faf2f
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-04 02:21:06 -0600

    module exporeted for required in app.js

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 require("./requireModule1")
+var fun1 = require("./moduleExport/exportFunction1")
+fun1()
---
+++ moduleExport/exportFunction1.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+  console.log("function called from exported module")
+}
