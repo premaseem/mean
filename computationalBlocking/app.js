3474ef2c4ccded62b384cfb7bc788b7f0b38b1f3
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-07 15:02:44 -0600

    blocking computation process with fibonacci

---
+++ computationalBlocking/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by asee2278 on 12/7/16.
+ */
+
+console.log("Start of process ");
+var process = require('./_fibonacci.js')
+process.recursive(10)
+
+console.log("End of process ");
+
