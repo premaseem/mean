be48a56734826589d027ff0f5fbce64dea6ed2f6
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-08 23:38:41 -0600

    add bootstrap file from superhero https://bootswatch.com/superhero/bootstrap.css

--- meanApp/app.js
+++ meanApp/app.js
@@ -2,15 +2,16 @@ var express = require('express')
 var path = require('path');
 var app = express()
 app.set('port',3000)
+app.use(express.static(path.join(__dirname,'public')))
 
 var server = app.listen(app.get('port'),function(){
     console.log("app started on port "+ server.address().port)
 })
 
-app.get('/', function(req, res){
-    console.log("get the home page or Root ");
-res.status(200).sendFile(path.join(__dirname,'public','index.html'))
-})
+//app.get('/', function(req, res){
+//    console.log("get the home page or Root ");
+//res.status(200).sendFile(path.join(__dirname,'public','index.html'))
+//})
 
 app.get('/json', function(req, res){
     console.log("get the home page or Root ");
