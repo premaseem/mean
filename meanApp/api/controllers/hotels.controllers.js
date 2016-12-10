284be5e070e2e44a8170aea17d040a71fecab40b
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-10 01:02:33 -0600

    implemented pagination using query string param http://localhost:3000/api/hotels?offset=0&count=3

--- meanApp/api/controllers/hotels.controllers.js
+++ meanApp/api/controllers/hotels.controllers.js
@@ -3,14 +3,28 @@
  */
 var hotelData = require('../data/hotel-data.json')
 
-module.exports.hotelsGetAll = function(req,res){
+module.exports.hotelsGetAll = function(req,res) {
     console.log('get all hotels')
-    res.status(200).json(hotelData);
+    console.log(req.query)
+    var offset = 1;
+    var count = 2;
+    if (req.query && req.query.offset) {
+
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count,10);
+    }
+    var returnData = hotelData.slice(offset,offset+count)
+
+    res.status(200).json(returnData);
 }
 
 module.exports.hotelsGetOne = function(req,res){
     var id = req.params.hotelId
     var thisHotel = hotelData[id]
+    console.log(req.query)
     console.log('get one hotel'+id)
     res.status(200).json(thisHotel);
 
