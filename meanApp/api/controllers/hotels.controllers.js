7d2ae1b0536958fd02c8568349e5e60b4ce2989d
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-10 00:24:54 -0600

    linked dummy data for hotels

--- meanApp/api/controllers/hotels.controllers.js
+++ meanApp/api/controllers/hotels.controllers.js
@@ -1,9 +1,9 @@
 /**
  * Created by asee2278 on 12/9/16.
  */
-path = require('path')
+var hotelData = require('../data/hotel-data.json')
 module.exports.hotelsGetAll = function(req,res){
 
     console.log('get all hotels')
-    res.status(200).send({"key":"hotel"});
+    res.status(200).json(hotelData);
 }
