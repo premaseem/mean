152f53c2d34085747ac5b8db5f61e52a1e85d89f
Aseem Jain <Aseem.Jain@rackspace.com>
2016-12-12 23:56:52 -0600

    post implementation

--- meanApp/api/controllers/hotels.controllers.js
+++ meanApp/api/controllers/hotels.controllers.js
@@ -52,9 +52,26 @@ module.exports.hotelsGetOne = function(req,res){
 }
 
 module.exports.hotelsAddOne = function(req, res) {
-    console.log("POST new hotel and node mon ");
-    console.log(req.body);
-    res
-        .status(200)
-        .json(req.body);
+
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    if(req.body && req.body.name && req.body.stars){
+        console.log("POST new hotel and node mon ");
+        console.log(req.body);
+        hotel = req.body;
+        hotel.stars = parseInt(req.body.stars,10);
+        collection.insertOne(hotel,function(err,response){
+            res
+                .status(201)
+                .json(response.ops);
+        });
+
+
+    }else {
+        console.log("error in payload ")
+        res
+            .status(400)
+            .json({"message":"Required data missing from body "})
+    }
+
 };
